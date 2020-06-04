8f8f846fc34d52a4f3066f265054355e5c906b0e
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-06-04 15:18:12 +1000

    init

---
+++ test.js
@@ -0,0 +1,34 @@
+var request = require('request'); // "Request" library
+
+var client_id = '5352be5b021042188d2c62155df50f5d'; // Your client id
+var client_secret = '2d83ee2f9cc84e60b54e0aeb5f0e1f3d'; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/isaac.elliott1804',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
