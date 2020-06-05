02d2c57a15bb4205b1c5f934b6f87825d89b30f4
Danny Field <58800432+DannyField@users.noreply.github.com>
2020-06-05 10:02:47 +1000

    Added lyric api and basic welcome screen

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-var request = require('request'); // "Request" library
+var request = require("request"); // "Request" library
 
-var client_id = '5352be5b021042188d2c62155df50f5d'; // Your client id
-var client_secret = '2d83ee2f9cc84e60b54e0aeb5f0e1f3d'; // Your secret
+var client_id = "5352be5b021042188d2c62155df50f5d"; // Your client id
+var client_secret = "2d83ee2f9cc84e60b54e0aeb5f0e1f3d"; // Your secret
 
 // your application requests authorization
 var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
+  url: "https://accounts.spotify.com/api/token",
   headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    Authorization:
+      "Basic " + new Buffer(client_id + ":" + client_secret).toString("base64"),
   },
   form: {
-    grant_type: 'client_credentials'
+    grant_type: "client_credentials",
   },
-  json: true
+  json: true,
 };
 
-request.post(authOptions, function(error, response, body) {
+request.post(authOptions, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-
     // use the access token to access the Spotify Web API
     var token = body.access_token;
     var options = {
-      url: 'https://api.spotify.com/v1/users/isaac.elliott1804',
+      url: "https://api.spotify.com/v1/users/isaac.elliott1804",
       headers: {
-        'Authorization': 'Bearer ' + token
+        Authorization: "Bearer " + token,
       },
-      json: true
+      json: true,
     };
-    request.get(options, function(error, response, body) {
+    request.get(options, function (error, response, body) {
       console.log(body);
     });
   }
